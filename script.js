1ef8d8a676a5bf2d28be14b966147e404d1eb137
Steven Zinck <steve@zinck.ca>
2023-04-05 20:45:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-window.addEventListener("load", function() {
-  function calculateBatteryLife() {
-    var batteryCapacity = document.getElementById("batteryCapacity").value;
-    var voltage = document.getElementById("voltage").value;
-    var averageDraw = document.getElementById("averageDraw").value;
+function calculateBatteryLife() {
+  var batteryCapacity = document.getElementById("batteryCapacity").value;
+  var voltage = document.getElementById("voltage").value;
+  var averageDraw = document.getElementById("averageDraw").value;
 
-    // convert battery capacity from mAh to Ah
-    batteryCapacity /= 1000;
+  // convert battery capacity from mAh to Ah
+  batteryCapacity /= 1000;
 
-    // calculate battery life
-    var batteryLife = batteryCapacity / (averageDraw * voltage);
+  // calculate battery life
+  var batteryLife = batteryCapacity / (averageDraw * voltage);
 
-    document.getElementById("batteryLife").innerHTML = "Estimated Battery Life: " + batteryLife.toFixed(2) + " hours";
-  }
-  
-  var calculateButton = document.getElementById("calculateButton");
-  calculateButton.addEventListener("click", calculateBatteryLife);
-});
+  document.getElementById("batteryLife").innerHTML = "Estimated Battery Life: " + batteryLife.toFixed(2) + " hours";
+}
+
+var calculateButton = document.getElementById("calculateButton");
+calculateButton.addEventListener("click", calculateBatteryLife);
